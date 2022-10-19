f6c4f354ae83aeeab4f38fa04aba2de0cf8a1182
Lauren Morgan <rg326@live.com>
2022-10-18 20:33:15 -0400

    added tokenizer to sample text in l_1 file

--- nltk/programmingknowledge_course/l_1.py
+++ nltk/programmingknowledge_course/l_1.py
@@ -2,3 +2,6 @@ import nltk
 nltk.download()
 
 text = """Welcome to programming knowledge. Let's start with our first tutorial on NLTK. We shall learn the basics of NLTK here!"""
+
+from nltk.tokenize import word_tokenize
+print(word_tokenize(text))
